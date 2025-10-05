92ee1f89e567c651d2183df8c6849e1a32c89539
Heshan Jayasekara <heshanjayasekara18@gmail.com>
2025-10-05 23:18:50 +0530

    project forem ui and project setting ui done

--- greenpluse/app/(root)/(MainTabs)/_layout.tsx
+++ greenpluse/app/(root)/(MainTabs)/_layout.tsx
@@ -128,6 +128,9 @@ const TabsLayout = () => {
       
       />
 
+      
+
+      
 
     </Tabs>
   )
--- greenpluse/app/(root)/_layout.tsx
+++ greenpluse/app/(root)/_layout.tsx
@@ -1,27 +1,48 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
-
-
-
 export default function RootLayout() {
   return (
     <>
-      <StatusBar hidden ={true} />
-     <Stack >
-      <Stack.Screen name="(MainTabs)" options={{ 
-                                         headerShown: false
-                                         }} 
-      />
+      <StatusBar hidden={true} />
+      <Stack>
+        <Stack.Screen 
+          name="(MainTabs)" 
+          options={{ headerShown: false }} 
+        />
 
-      <Stack.Screen name="(subTabs)" options={{ 
-                                         headerShown: false
-                                         }} 
-      />
+        <Stack.Screen 
+          name="(subTabs)" 
+          options={{ headerShown: false }} 
+        />
 
-      <Stack.Screen name="signIn" options={{ headerShown: false}} />  
-                         
-    </Stack>
-   </>
+        <Stack.Screen 
+          name="signIn" 
+          options={{ headerShown: false }} 
+        />  
+
+        <Stack.Screen 
+          name="ProjectSetting" 
+          options={{ headerShown: false }} 
+        />
+
+
+        <Stack.Screen 
+          name="RequestProject" 
+          options={{ headerShown: false }} 
+        />
+
+        <Stack.Screen 
+          name="ProjectDetails" 
+          options={{ headerShown: false }} 
+        />
+
+
+        
+      </Stack>
+
+        
+      
+    </>
   );
 }
